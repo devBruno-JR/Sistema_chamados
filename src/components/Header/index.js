d26d4a21b7c0497cf2a50171c0dd15db3209b785
Bruno <devbrunojr@gmail.com>
2025-10-08 21:56:25 -0300

    header sem resposividade

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,37 @@
-// import '.'
+import avatarImg from'../../assets/avatar.png'
+import { useContext } from 'react';
+import { AuthContext } from '../../Context/auth';
+import { Link } from 'react-router-dom';
+import { FiHome, FiUser, FiSettings } from 'react-icons/fi';
+import './header.css'
 
 
 export default function Header() {
+  const {user} = useContext(AuthContext)
  return (
    <div className="sidebar">
   <div>
     <img
-    src=""
+    src={user.avartarUrl === null ? avatarImg : user.avartarUrl}
     alt="Foto do usuario"
     />
   </div>
+<Link to='/dashboard'>
+<FiHome color='#fff' size={24}  />
+Chamados
+</Link>
+
+<Link to='/customers'>
+<FiUser color='#fff' size={24}  />
+Clientes
+</Link>
+
+<Link to="/profile" >
+<FiSettings color='#fff' size={24}  />
+Prefil
+</Link>
+
+
    </div>
  );
 }
